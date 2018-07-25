9018307a52d3b58f8afeae344858982d7567785a
Mumen alkhatib <mumen_alkhatib@yahoo.com>
2018-07-25 17:31:57 +0300

    homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,15 +9,28 @@ export class HomePage {
 
   searchTerm: string = '';
   items: any;
+  medicine:string;
+  insuranceProviders = [
+    {name: "Trust", imageUrl: "trust1.jpg"},
+    {name: "Takaful", imageUrl: "tkafl1.png"},
+    {name: "Globemed", imageUrl: "globemed1.jpg"},
+    {name: "Nathealth", imageUrl: "nathealth1.jpg"},
+    {name: "Mashreq", imageUrl: "mashreq1.png"},
+    {name: "Ahlia", imageUrl: "ahlia1.png"},
+  ];
+  selectedInsurance:string;
 
   constructor(public navCtrl: NavController,) {
-
   }
 
   ionViewDidLoad() {
  
     this.setFilteredItems();
 
+}
+
+selectInsurance(insurance1) {
+  this.selectedInsurance = insurance1.name;
 }
 
 setFilteredItems() {
