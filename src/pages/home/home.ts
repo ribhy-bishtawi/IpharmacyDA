4f8fb5a09682fa5f83ca240d1fb9f31bc724dbaa
ravina96 <ravinajain@gmail.com>
2018-07-25 10:36:08 +0300

    Adding feedback page button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FeedbackPage } from '../feedback/feedback';
 
 @Component({
   selector: 'page-home',
@@ -12,6 +13,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController,) {
 
+   
+  }
+  openfeedback(){
+    this.navCtrl.push(FeedbackPage)
   }
 
   ionViewDidLoad() {
